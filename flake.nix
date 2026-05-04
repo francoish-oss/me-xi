11c1040845d06e8aaaff049ba5009634566ce94f
François H <contact@capvec.dev>
2026-05-04 19:11:50 +0000

    My first flake

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Me-xi";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.dell-xps-9380-13-4k = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./configuration.nix
+        ./hardware-configuration.nix
+      ];
+    };
+  };
+}
