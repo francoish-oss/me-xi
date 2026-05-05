7d8e895109bddc5c75dcf6e96516d16624f20924
François H <contact@capvec.dev>
2026-05-05 13:02:46 +0000

    Add Home Manager, Plasma Manager and stylix

--- flake.nix
+++ flake.nix
@@ -4,6 +4,19 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    plasma-manager = {
+      url = "github:nix-community/plasma-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
+
+    stylix.url = "github:nix-community/stylix";
   };
 
   outputs = { self, nixpkgs, nixos-hardware, ... }@inputs: {
