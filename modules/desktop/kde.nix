8269ba43db0811bb755fa56b0a2b84dab2876909
François H <contact@capvec.dev>
2026-05-05 00:05:19 +0000

    Once again the unchecked was not selected

---
+++ modules/desktop/kde.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, ... }:
+
+{
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+  ];
+}
