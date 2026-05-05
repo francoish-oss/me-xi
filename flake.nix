bceeeade8e63da7f4a2e5c3a1950eef13e53b4aa
François H <contact@capvec.dev>
2026-05-05 11:14:39 +0000

    Syntax error

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    inputs.nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
   outputs = { self, nixpkgs, nixos-hardware, ... }@inputs: {
