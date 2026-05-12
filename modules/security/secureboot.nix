fa2645c193b1b1c994df143cda4755d96ca855df
François H <contact@capvec.dev>
2026-05-12 14:00:39 +0000

    a

--- modules/security/secureboot.nix
+++ modules/security/secureboot.nix
@@ -1,12 +1,11 @@
-{ pkgs, lib, inputs, ... }: {
-
-  # Lanzaboote currently replaces the systemd-boot module.
-  # This setting is usually set to true in configuration.nix
-  # generated at installation time. So we force it to false
-  # for now.
+{ pkgs, lib, ... }: {
   boot.loader.systemd-boot.enable = lib.mkForce false;
   boot.lanzaboote = {
     enable = true;
     pkiBundle = "/var/lib/sbctl";
   };
+
+  environment.systemPackages = [
+    pkgs.sbctl
+  ];
 }
