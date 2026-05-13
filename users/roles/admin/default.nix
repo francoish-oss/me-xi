222a9a53266cf0c66d2f1bad2c19168493bf71b2
François H <contact@capvec.dev>
2026-05-13 20:42:16 +0000

    Test with home persistance module

--- users/roles/admin/default.nix
+++ users/roles/admin/default.nix
@@ -9,20 +9,20 @@
 
   home.persistence."/persist${config.home.homeDirectory}" = {
     hideMounts = true;
-    users."${config.home.username}" = {
-      directories = [
-        "Documents"
-        "Pictures"
-        "Videos"
-        ".config/cosmic"
-        ".local/share/cosmic"
-        ".local/state/cosmic"
-        ".local/share/keyrings" # Need for wifi
-      ];
-      files = [
-        ".config/cosmic-initial-setup-done"
-      ];
-    };
+    # users."${config.home.username}" = {
+    directories = [
+      "Documents"
+      "Pictures"
+      "Videos"
+      ".config/cosmic"
+      ".local/share/cosmic"
+      ".local/state/cosmic"
+      ".local/share/keyrings" # Need for wifi
+    ];
+    files = [
+      ".config/cosmic-initial-setup-done"
+    ];
+    # };
   };
 
 }
