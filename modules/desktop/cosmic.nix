97f9d97f0beea6369c5a300f7392fcd9e2d85870
François H <contact@capvec.dev>
2026-05-13 20:00:03 +0000

    Update cosmic.nix

--- modules/desktop/cosmic.nix
+++ modules/desktop/cosmic.nix
@@ -4,12 +4,14 @@
   services.desktopManager.cosmic.enable = true;
 
   # If Impermanence add COSMIC Settings to persist
-  environment.persistence."/persist" = lib.mkIf config.mySystem.impermanence.enable {
-    hideMounts = true;
-    directories = [
-      "/var/lib/cosmic"
-      ".config/cosmic"
-    ];
-  };
+  environment.persistence."/persist" =
+    lib.mkIf (lib.hasAttr "/persist" config.environment.persistence)
+      {
+        hideMounts = true;
+        directories = [
+          "/var/lib/cosmic"
+          ".config/cosmic"
+        ];
+      };
 
 }
