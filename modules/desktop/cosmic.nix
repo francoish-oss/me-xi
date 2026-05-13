d864a787f07b79f3379ca18be5b2fed5b3dac4ce
François H <contact@capvec.dev>
2026-05-13 20:02:38 +0000

    Conditional persistance

--- modules/desktop/cosmic.nix
+++ modules/desktop/cosmic.nix
@@ -1,17 +1,20 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  options,
+  ...
+}:
 
 {
   services.desktopManager.cosmic.enable = true;
 
   # If Impermanence add COSMIC Settings to persist
-  environment.persistence."/persist" =
-    lib.mkIf (lib.hasAttr "/persist" config.environment.persistence)
-      {
-        hideMounts = true;
-        directories = [
-          "/var/lib/cosmic"
-          ".config/cosmic"
-        ];
-      };
+  environment.persistence."/persist" = lib.mkIf (options.environment ? persistence) {
+    hideMounts = true;
+    directories = [
+      "/var/lib/cosmic"
+      ".config/cosmic"
+    ];
+  };
 
 }
