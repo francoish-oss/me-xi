452ef09a4b3919bd649ba4e087427bf7d037cf73
François H <contact@capvec.dev>
2026-05-14 19:44:41 +0000

    Update bitwarden.nix

--- apps/bitwarden.nix
+++ apps/bitwarden.nix
@@ -16,11 +16,10 @@
     enable = true;
     extraConfig = ''
       Host *
-        # Hard-blindfold SSH from local files
         IdentityFile /dev/null
         IdentitiesOnly yes
-        # Explicitly point to the rbw socket
-        IdentityAgent /run/user/${toString config.home.uid}/rbw/ssh-agent-socket
+        # Use the environment variable directly
+        IdentityAgent ''${XDG_RUNTIME_DIR}/rbw/ssh-agent-socket
     '';
   };
 
