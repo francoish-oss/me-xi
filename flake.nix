d389c8035609ac950d6842acbf6dc9551364c04d
François H <contact@capvec.dev>
2026-05-15 14:26:28 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -74,6 +74,7 @@
           ];
           extraModules = [
             ./modules/desktop/cosmic.nix
+            ./modules/desktop/cosmic-greeter.nix
             ./users/defs/f.nix
             ./users/defs/fadmin.nix
           ];
