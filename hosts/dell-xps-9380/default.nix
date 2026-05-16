5f5de97af50f87d6ab128b61a5dcbe014e518a2f
François H <contact@capvec.dev>
2026-05-16 18:18:43 +0000

    Update default.nix

--- hosts/dell-xps-9380/default.nix
+++ hosts/dell-xps-9380/default.nix
@@ -9,7 +9,15 @@
     ./bluetooth.nix
   ];
 
-  hardware.video.hidpi.enable = true;
+  boot.loader.grub = {
+    enable = true;
+
+    # Higher resolution but still readable
+    gfxmodeEfi = "1920x1080";
+
+    # Optional: use same resolution for boot splash
+    gfxpayloadEfi = "keep";
+  };
 
   system.stateVersion = "25.11";
 }
