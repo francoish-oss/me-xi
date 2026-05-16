f76766048874bfdc0a48520291d3db8529ed4dfb
François H <contact@capvec.dev>
2026-05-16 16:45:13 +0000

    Update tty-greeter.nix

--- modules/desktop/tty-greeter.nix
+++ modules/desktop/tty-greeter.nix
@@ -1,9 +1,8 @@
 { ... }:
 
 {
-  programs.bash.loginShellInit = ''
-    # If we are physically on TTY1 and COSMIC isn't running yet, launch it!
-    if [ "$XDG_VTNR" = "1" ] && [ -z "$WAYLAND_DISPLAY" ]; then
+  environment.interactiveShellInit = ''
+    if [ -z "$DISPLAY" ] && [ "$(tty)" = "/dev/tty1" ]; then
       exec start-cosmic
     fi
   '';
