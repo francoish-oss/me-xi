0b463d6dbc71b63ccbd5d796b4e8025b56355f5f
François H <contact@capvec.dev>
2026-05-16 16:03:30 +0000

    Update tty-greeter.nix

--- modules/desktop/tty-greeter.nix
+++ modules/desktop/tty-greeter.nix
@@ -1,5 +1,11 @@
 { ... }:
 
 {
-  programs.bash.loginShellInit = "exec start-cosmic";
+  programs.bash.loginShellInit = ''
+    # Only run if we are on the first text terminal (login)
+    # and a Wayland session isn't already running.
+    if [ "$(tty)" = "/dev/tty1" ] && [ -z "$WAYLAND_DISPLAY" ]; then
+      exec start-cosmic
+    fi
+  '';
 }
