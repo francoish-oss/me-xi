4bdea43b11bed1be949fec28dd27bba42a20f41a
François H <contact@capvec.dev>
2026-05-17 16:16:39 +0000

    Update firefox.nix

--- apps/firefox.nix
+++ apps/firefox.nix
@@ -3,19 +3,19 @@
 {
   programs.firefox = {
     enable = true;
-
     package = pkgs.firefox;
 
     profiles.default = {
       isDefault = true;
 
       settings = {
+        "widget.use-xdg-desktop-portal.file-picker" = 1;
         "browser.aboutConfig.showWarning" = false;
         "dom.security.https_only_mode" = true;
         "privacy.trackingprotection.enabled" = true;
         "privacy.resistFingerprinting" = true;
       };
-
     };
   };
+
 }
