0ecce080c3e4f16a4bab59a6018f5e4c39fe8f13
François H <contact@capvec.dev>
2026-05-17 05:07:23 +0000

    Update nix-switch-from-git.nix

--- pkgs/nix-switch-from-git.nix
+++ pkgs/nix-switch-from-git.nix
@@ -9,60 +9,19 @@ let
     ];
     text = ''
       echo "🔓 Unlocking /boot..."
-      pkexec mount -o remount,rw /boot
+      sudo mount -o remount,rw /boot
 
       echo "❄️ Starting NixOS Rebuild..."
-      pkexec nixos-rebuild switch --flake "github:francoish-oss/me-xi#dell-xps-9380" --refresh
+      sudo nixos-rebuild switch --flake "github:francoish-oss/me-xi#dell-xps-9380" --refresh
 
       echo "🔒 Locking /boot..."
-      pkexec mount -o remount,ro /boot
+      sudo mount -o remount,ro /boot
 
       echo "✅ Done! System is updated and /boot is secured."
       read -rp "Press enter to close..."
     '';
-    # text = ''
-    #   echo "🔓 Unlocking /boot..."
-    #   sudo mount -o remount,rw /boot
-
-    #   echo "❄️ Starting NixOS Rebuild..."
-    #   sudo nixos-rebuild switch --flake "github:francoish-oss/me-xi#dell-xps-9380" --refresh
-
-    #   echo "🔒 Locking /boot..."
-    #   sudo mount -o remount,ro /boot
-
-    #   echo "✅ Done! System is updated and /boot is secured."
-    #   read -rp "Press enter to close..."
-    # '';
   };
 in
 {
-
   environment.systemPackages = [ updateScript ];
-
-  # security.wrappers.nix-switch-from-git = {
-  #   owner = "root";
-  #   group = "root";
-  #   # This dynamically grabs the absolute store path of your compiled script:
-  #   source = "${updateScript}/bin/nix-switch-from-git-internal";
-  #   setuid = true;
-  #   setgid = false;
-  # };
-
-  # home.packages = [
-  #   updateScript
-  # ];
-
-  # users.groups.nix-switch-from-git = { };
-  # security.sudo.extraRules = [
-  #   {
-  #     groups = [ "nix-switch-from-git" ];
-
-  #     commands = [
-  #       {
-  #         command = "/run/current-system/sw/bin/systemctl start update-from-git@*";
-  #         options = [ "NOPASSWD" ];
-  #       }
-  #     ];
-  #   }
-  # ];
 }
