cffec799adb3178c57c7546e02ddabc0301b92ed
François H <contact@capvec.dev>
2026-05-18 19:13:19 +0000

    Update netbird.nix

--- modules/desktop/netbird.nix
+++ modules/desktop/netbird.nix
@@ -3,7 +3,7 @@
 {
   services.netbird.clients.wt0 = {
     hardened = true;
-
+    port = 51820;
     ui.enable = true;
     # Automatically opens the necessary ports in the NixOS firewall
     openFirewall = true;
