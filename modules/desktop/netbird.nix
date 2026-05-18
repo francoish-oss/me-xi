27e49cd1716d22fcab1b4da1062cebc66a335824
François H <contact@capvec.dev>
2026-05-18 19:10:26 +0000

    Update netbird.nix

--- modules/desktop/netbird.nix
+++ modules/desktop/netbird.nix
@@ -1,18 +1,11 @@
 { config, pkgs, ... }:
 
 {
-  # Enable the NetBird client service
   services.netbird.clients.wt0 = {
-    enable = true;
+    hardened = true;
 
-    # Set to true if you are on a desktop and want the Graphical UI
-    ui.enable = false;
-
+    ui.enable = true;
     # Automatically opens the necessary ports in the NixOS firewall
     openFirewall = true;
   };
-
-  # Optional: For Netbird's mesh DNS resolution to work smoothly,
-  # it is highly recommended to enable systemd-resolved
-  services.resolved.enable = true;
 }
